ae6679a06e59e4bad6fe89887337710c777c40e5
Huy Nguyen <huy.m.nguyen@live.com>
2019-08-07 14:34:00 -0700

    Add var and require to liri.js

--- liri.js
+++ liri.js
@@ -1,5 +1,14 @@
+// requires to read and set any environment variables for dotenv package
 require('dotenv').config();
 
 var keys = require('./keys.js');
+var colors = require('colors');
+var axios = require('axios');
+var fs = require('fs');
+var spotify = require('node-spotify-api');
+var moment = require('moment');
 
 var spotify = new spotify(keys.spotify);
+
+var liriSearch = process.argv[2];
+var liriTerm = process.argv.slice(3).join(' ');
